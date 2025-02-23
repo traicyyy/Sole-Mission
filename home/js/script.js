59b8e7c2cc7d4811f948ce33da5761a29551fc87
Tracie Tomon <tctomon@student.apc.edu.ph>
2025-02-24 01:05:17 +0800

    update

--- home/js/script.js
+++ home/js/script.js
@@ -1,21 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
-    const sections = document.querySelectorAll("section");
     const navLinks = document.querySelectorAll(".nav-link");
 
-    window.addEventListener("scroll", () => {
-        let current = "";
-        sections.forEach((section) => {
-            const sectionTop = section.offsetTop;
-            if (pageYOffset >= sectionTop - 60) {
-                current = section.getAttribute("id");
-            }
-        });
-
-        navLinks.forEach((link) => {
+    navLinks.forEach((link) => {
+        if (link.href === window.location.href) {
+            link.classList.add("active");
+        } else {
             link.classList.remove("active");
-            if (link.getAttribute("href").substring(1) === current) {
-                link.classList.add("active");
-            }
-        });
+        }
     });
 });
