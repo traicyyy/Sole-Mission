1bdaf2883f270d345f8f60ea2cb934890b947c5c
Tracie Tomon <tctomon@student.apc.edu.ph>
2025-03-01 18:07:29 +0800

    update

--- home/js/book-now.js
+++ home/js/book-now.js
@@ -23,7 +23,7 @@ createApp({
         const showConfirmation = ref(false); 
         
         const serviceOptions = ref([]);
-        
+
         const goBack = () => {
             if (window.history.length > 1) {
                 window.history.back(); 
@@ -31,7 +31,7 @@ createApp({
                 window.location.href = "index.html";  
             }
         };
-        
+
         function updateServiceNames() {
             if (serviceType.value === "Cleaning") {
                 serviceOptions.value = ["Deep Clean", "Sole Unyellowing"];
@@ -50,57 +50,8 @@ createApp({
             }
         }
 
-        function calculateTotal() {
-            let prices = {
-                Cleaning: {
-                    "Deep Clean": 350,
-                    "Sole Unyellowing": 750                    
-                },
-                Restoration: {
-                    "Full Repaint": 1200,
-                    "Full Outsole Reglue": 1200,
-                    "Full Midsole Reglue": 1500,
-                    "Sole Replacement": 3500,
-                    "Sole Stitch": 300,
-                    "Partial Repaint": 300,
-                    "Partial Reglue": 400                    
-                }                
-            };
-            
-            if (serviceType.value && serviceName.value && prices[serviceType.value] && prices[serviceType.value][serviceName.value]) {
-                totalPayment.value = `₱${prices[serviceType.value][serviceName.value] * numItems.value}`;
-            }            
-        }
-
-        function resetForm() {
-            firstName.value = "";
-            lastName.value = "";
-            contactNumber.value = "";
-            email.value = "";
-            shoeBrandModel.value = "";
-            serviceType.value = "";
-            serviceName.value = "";
-            numItems.value = 1;
-            totalPayment.value = "₱0";
-            paymentMethod.value = "";
-            deliveryType.value = "";
-            address.street = "";
-            address.city = "";
-            address.postalCode = "";
-            message.value = "";
-            agreeToTerms.value = false;
-            serviceOptions.value = [];
-        }
-
         function submitForm() {
             showConfirmation.value = true;  
-        }
-
-        function confirmBooking() {
-            alert("Booking confirmed successfully!");
-            resetForm();
-            showConfirmation.value = false;
-            window.location.href = "index.html"; 
         }
 
         return {
@@ -120,12 +71,9 @@ createApp({
             agreeToTerms,
             serviceOptions,
             updateServiceNames,
-            calculateTotal,
             submitForm,
-            confirmBooking,
             showConfirmation,
-            goBack  
+            goBack
         };
-
     }
 }).mount("#app");
